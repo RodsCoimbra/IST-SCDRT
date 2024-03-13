f4ddaf299c8ee2e3c896b98417066fd31afb24f0
RodsCoimbra <rodscoimbra@gmail.com>
2024-03-13 13:15:02 +0000

    Changes gráfico m (gain), se quiser voltar atrás

--- Code/Teste/Ganho/ganho.m
+++ Code/Teste/Ganho/ganho.m
@@ -40,29 +40,30 @@ sprintf("%f, %f", er, m(i))
 close all
 figure();
 color = get(gca,'ColorOrder');
+color = [color(5,:)];
 i = 0;
 hold on
-legend_entries = cell(1, 8);
-for j = 1:10:(len/15)
-    i = i+1;
-    k = (j-1) * 15;
-    plot(value((k+1):(k+15),1), value((k+1):(k+15),2), 'o', 'Color',color(i, :),'MarkerSize',5)
-    %title(["Ganho para m =" num2str(m(j))])
-    legend_entries{i*2-1} = sprintf('m= %f', m(j));
-    xlabel("Duty-cycle")
-    ylabel("Lux")
-    legend_entries{i*2} = sprintf(''); % Create legend string
-    b1 = value((k+5):(k+15),1)\ value((k+5):(k+15),2);
-    plot(value((k+1):(k+15),1), b1*value((k+1):(k+15),1), '--' ,'Color',color(i,:));
-end
+legend_entries = cell(1, 2);
+% for j = 1:10:(len/15)
+%     i = i+1;
+%     k = (j-1) * 15;
+%     plot(value((k+1):(k+15),1), value((k+1):(k+15),2), 'o', 'Color',color(i, :),'MarkerSize',5)
+%     title(["Ganho para m =" num2str(m(j))])
+%     legend_entries{i*2-1} = sprintf('m= %f', m(j));
+%     xlabel("Duty-cycle")
+%     ylabel("Lux")
+%     legend_entries{i*2} = sprintf(''); % Create legend string
+%     b1 = value((k+1):(k+15),1)\ value((k+1):(k+15),2);
+%     plot(value((k+1):(k+15),1), b1*value((k+1):(k+15),1), '--' ,'Color',color(i,:));
+% end
     i = i+1;
     k = 1 * 15;
-    plot(value((k+1):(k+15),1), value((k+1):(k+15),2), 'x', 'Color',color(i, :),'MarkerSize',9)
+    plot(value((k+1):(k+15),1), value((k+1):(k+15),2), 'kx','MarkerSize',9)
     %title(["Ganho para m =" num2str(m(j))])
     legend_entries{i*2-1} = sprintf('m= %f', m(2));
     xlabel("Duty-cycle")
     ylabel("Lux")
     legend_entries{i*2} = sprintf(''); % Create legend string
-    b1 = value((k+5):(k+15),1)\ value((k+5):(k+15),2);
-    plot(value((k+1):(k+15),1), b1*value((k+1):(k+15),1), '--' ,'Color',color(i,:));
-    legend(legend_entries);
+    b1 = value((k+1):(k+15),1)\ value((k+1):(k+15),2);
+    plot(value((k+1):(k+15),1), b1*value((k+1):(k+15),1), '-' ,'Color',color(1,:), 'Linewidth', 1.5);
+    legend(["Data when m = 0.89" "Linear regression for m = 0.89"]);
--- Code/Teste/Tau/Copy_of_tau_experimental.m
+++ Code/Teste/Tau/Copy_of_tau_experimental.m
@@ -1,6 +1,6 @@
 clear
 close all
-tau = load("tau_exp_2047.csv");
+tau = load("Tau_exp_2047_4095.csv");
 plot((tau(:,1)-tau(101,1))*1e-6, tau(:,2))
 grid on
 value = 0.63 * (mean(tau(3600:4101,2))-mean(tau(1:100,2)));
